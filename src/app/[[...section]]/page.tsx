17078a478121652593fffa5dab621a4d218e8abd
Jehaad AL-Johani <JEHAADXZX518@gmail.com>
2025-05-28 00:13:44 +0300

    Added: Title

--- src/app/[[...section]]/page.tsx
+++ src/app/[[...section]]/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     <>
       <CreateListPopup />
       <div className="flex flex-col gap-10 items-center w-screen h-screen pt-60 bg-zinc-950">
+        <h1 className="text-3xl font-semibold">iTunes API Integration</h1>
+        <h2 className="text-lg opacity-80 -mt-7">By Jehaad AL-Johani</h2>
         <ReactQueryClientProvider>
           <div className="flex flex-col items-center gap-6 w-120 pb-40">
             <Lists />
